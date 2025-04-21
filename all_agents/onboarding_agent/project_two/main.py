6260230a55403a142b2d8ee73f62db5db25a31c3
ymgong66966 <ymgong3616@gmail.com>
2025-04-21 02:24:16 -0500

    multi agent

--- all_agents/onboarding_agent/project_two/main.py
+++ all_agents/onboarding_agent/project_two/main.py
@@ -1,12 +1,13 @@
 import sys
 import os.path
-
-# Add the all_agents directory to the Python path
-parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", ".."))
-all_agents_dir = os.path.join(parent_dir, "all_agents")
-sys.path.append(all_agents_dir)
-parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", ".."))
-sys.path.append(parent_dir)
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+# # Add the all_agents directory to the Python path
+# parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", ".."))
+# all_agents_dir = os.path.join(parent_dir, "all_agents")
+# all_agents_dir = os.path.join(parent_dir, "onboarding_agent")
+# sys.path.append(all_agents_dir)
+# parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", ".."))
+# sys.path.append(parent_dir)
 from onboarding_agent.project_two.utils.onboarding import create_graph
 
 graph = create_graph()
