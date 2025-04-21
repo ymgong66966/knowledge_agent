4a41d26dd5415f370cfd7ab0dfc07799fb8abe49
ymgong66966 <ymgong3616@gmail.com>
2025-04-21 02:05:09 -0500

    multi agent

--- all_agents/onboarding_agent/project_two/main.py
+++ all_agents/onboarding_agent/project_two/main.py
@@ -1,3 +1,19 @@
+import sys
+import os.path
+
+# Add the all_agents directory to the Python path
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", ".."))
+all_agents_dir = os.path.join(parent_dir, "all_agents")
+sys.path.append(all_agents_dir)
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", ".."))
+sys.path.append(parent_dir)
 from onboarding_agent.project_two.utils.onboarding import create_graph
 
 graph = create_graph()
+# graph = create_graph()
+# Add the parent directory (all_agents) to the Python path
+
+
+# from onboarding_agent.project_two.utils.onboarding import create_graph
+
+# graph = create_graph()
